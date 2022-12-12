968bfe8a18e41d25065dbdc3d38d9134f56f9bee
shubham-sigmoid <shubham.chandra@sigmoidanalytics.com>
2022-12-12 14:46:48 +0530

    change macro for source test

--- macros/source_for_test.sql
+++ macros/source_for_test.sql
@@ -5,12 +5,11 @@
 
       {% if target.name == 'unit-test' %}
 
-            {%- set test_source_relation = adapter.get_relation(
-                  database = test_ref_relation.database,
-                  schema = 'unit_testing',
-                  identifier = 'test_' ~ test_ref_relation.identifier) 
-            -%}
             
+            
+            {%- set test_source_relation = source('test_' ~ source_schema, 'test_' ~ source_name)
+            -%}
+
             {{ return(test_source_relation) }}
 
       {% else %}
