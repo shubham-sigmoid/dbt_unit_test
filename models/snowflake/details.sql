e574a709005e478bd26fe06668d74f4e135e1831
shubham-sigmoid <shubham.chandra@sigmoidanalytics.com>
2022-12-12 14:49:40 +0530

    update model

--- models/snowflake/details.sql
+++ models/snowflake/details.sql
@@ -6,9 +6,9 @@
 
 
 
-SELECT id,
-    name,
-    city
+SELECT customer.id,
+    customer.name,
+    contact.city
 FROM {{ source_for_test('snowflake', 'customer') }} customer
     LEFT JOIN {{ source_for_test('snowflake', 'contact') }} contact ON customer.id = contact.id
 
